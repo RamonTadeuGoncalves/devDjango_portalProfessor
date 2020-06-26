cbdd6337f53262c864b883745043d1cc85cecd7f
Ramon Tadeu Goncalves <ramon@ramon-debian.ramon>
2020-06-26 11:00:43 -0300

    Configured the labels from forms, and corrected the url after edit a register

--- portalProfessor/forms.py
+++ portalProfessor/forms.py
@@ -5,3 +5,10 @@ class ProfessorForm(forms.ModelForm):
     class Meta:
         model = Professor
         fields = ('matricula', 'nome', 'endereco', 'telefone', 'titulacao', )
+        labels = {
+            'matricula':'Matrícula',
+            'nome':'Nome',
+            'endereco':'Endereço',
+            'telefone':'Telefone',
+            'titulacao':'Titulação',
+        }
